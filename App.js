1c4490fea57fbf9e1e0b674d0fe6783eeb4f0905
Jonatan de Lima <jonatanlima852@gmail.com>
2024-08-28 22:56:36 -0300

    create component ImageViewer

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
+import ImageViewer from './components/ImageViewer';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const PlaceholderImage = require('./assets/images/background-image.png')
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+    borderBlockColor: 'red'
+  }, 
 });
